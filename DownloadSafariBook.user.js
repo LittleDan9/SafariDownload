c0770702a57458a7327222d9a2f437b661157be8
Daniel R. Little <dan@littledan.com>
2020-10-20 23:47:10 -0400

    WIP: Chaining Promises

--- DownloadSafariBook.user.js
+++ DownloadSafariBook.user.js
@@ -12,32 +12,18 @@
 // @require      https://npmcdn.com/ejs/ejs.min.js
 // ==/UserScript==
 
-const jepub = new jEpub();
-var bookInfo;
-var bookDetails;
-var bookId;
-function getBookInfo(){
-    const regEx = /\/[0-9]{13}\//g
-    const baseURL = "https://learning.oreilly.com";
-    var bookId = window.location.href.match(regEx)[0].replaceAll('/','')
-    $.get(baseURL + "/nest/epub/toc/?book_id=" + bookId, handleBookInfo);
-}
-
-function handleBookInfo(data){
-    bookInfo = data;
-    getBookDetails();
-}
-
-function getBookDetails(){
-    $.get(baseURL + data.detail_url, function(date){bookDetails = data;});
-}
-
-
 (function() {
     'use strict';
-    getBookInfo();
-    // const jepub = new jEpub();
-
+    const jepub = new jEpub();
+    var bookInfo;
+    var bookDetails;
+    const regEx = /\/[0-9]{13}\//g
+    const baseURL = "https://learning.oreilly.com";
+    var bookId = window.location.href.match(regEx)[0].replace(/\//g,'')
+    if(bookId === undefined){
+        console.error("Unable to extract book Id.");
+        return;
+    }
     $.get(baseURL + "/nest/epub/toc/?book_id=" + bookId, function(data){        
         $.get(baseURL + data.detail_url, function(detail){
             var html = $($.parseHTML(detail));
@@ -52,80 +38,65 @@ function getBookDetails(){
                 date: Date.parse(data.pub_data)
             });
             jepub.uuid(bookId);
-            //console.log(data.items)
+
+            // Download Chapter Content
+            var chapterPromise = [];
             for(var i = 0; i < data.items.length; i++){
-                $.get(baseURL + data.items[i].url, function(result){
-                    // Download Chapter Content
-                    //console.log(result);
-                    $.get(result.content, function(HTML){
-                        var htmlSrc = HTML;
-                        var content = $('<div>' + htmlSrc + '</div>');
-                        //Inject Stylesheets
-                        result.site_styles
-                        for(let j = 0; j < result.stylesheets.length; j++){
-                            result.stylesheets[j].url
-                        }
-                        // Replace all imge tags in the content with the code for the epub builder
-                        if(result.images.length > 0){
-                            setEvents(result.images.length);
-                            console.log(imgEvents);
-                            $.each(result.images, function(key, value){
-                                var xhr = new XMLHttpRequest();
-                                xhr.onreadystatechange = function(){
-                                    if (xhr.readyState === XMLHttpRequest.DONE){
-                                        if (this.readyState == 4 && this.status == 200){
-                                            //console.log(this.response, typeof this.response);
-                                            jepub.image(this.response, value)
-                                        }
-                                        sendProgress();
-                                    }
-                                }
-                                xhr.open('GET', result.asset_base_url + value);
-                                xhr.responseType = 'blob';
-                                xhr.send();
-                            });
-                            pollEvents();
-                        }
-                        //jepub.image(data: object, IMG_ID: string)
-                        content.find("img").replaceWith("<%= " + $(this).attr("src") + " %>");
-                        htmlSrc = content.html();
-                        // Add Chapter to book
-                        jepub.add(result.label, htmlSrc, result.order);
-                    });
-                });
+                chapterPromise.push(getChapter(data.items[i]));
             }
+
+            $.when(chapterPromise).then(function(){console.log("Chapter Compelte");});
         });
     });
     //download("https://learning.oreilly.com/nest/epub/toc/?book_id=" + bookId, "/test/text.json");
-})();
 
-function download(url, filename) {
-    fetch(url).then(function(t) {
-        return t.blob().then((b)=>{
-            var a = document.createElement("a");
-            a.href = URL.createObjectURL(b);
-            a.setAttribute("download", filename);
-            a.click();
-        }
-      );
-    });
-}
-var imgEvents = 0;
-function setEvents(count){
-    //console.log("Set Event: " + count);
-    imgEvents = imgEvents + count;
-}
-function sendProgress(){
-    //console.log("sendProgress");
-    //console.log(imgEvents);
-    imgEvents--;
-}
-
-function pollEvents(){
-    //console.log("pollEvents");
-    if(imgEvents > 0){
-        setTimeout(pollEvents, 300)
-    }else{
-        console.log("end");
+    function getChapter(chapter){
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: baseURL + chapter.url,
+                success: function(chapterDetails){ 
+                    $.get(chapterDetails.content, function(chapterHTML){
+                        processChapter(chapterDetails.title, chapter.order, chapterDetails.images, chapterDetails.asset_base_url, chapterHTML);
+                    })
+                    resolve(chapterDetails);
+                },
+                error: function(error){reject(error)}
+            })
+        });
     }
-}
+    function processChapter(chapterName, chapterOrder, images, imgBaseURL, chapterHTML){
+        var chapterDOMTree = $('<div>' + chapterHTML + '</div>');
+        console.log(chapterOrder + ": " + chapterName)
+        if(images.length > 0){
+            console.log("Image Count: " + images.length);
+            var imgPromises = [];
+            $.each(images, function(key, value){
+                // Add epub image replace code foreach img to DOM Tree
+                chapterDOMTree.find("img[src='" + value + "']").replaceWith("<%= image[" + value + "] %>");             
+            });
+
+            // When all the images have downloaded and have been stored to the epub object
+            // update the chapter HTML with the replace format for ebup.
+            // Add the chapter to the epub.
+            $.when(imgPromises).then(function(){
+                console.log("Images Processed");
+                jepub.add(chapterName, chapterDOMTree.html(), chapterOrder);  
+            })            
+        }else{
+            console.log("No Images");
+            jepub.add(chapterName, chapterDOMTree.html(), chapterOrder);
+        }
+    }
+
+    function download(url, filename) {
+        fetch(url).then(function(t) {
+            return t.blob().then((b)=>{
+                var a = document.createElement("a");
+                a.href = URL.createObjectURL(b);
+                a.setAttribute("download", filename);
+                a.click();
+            }
+        );
+        });
+    }
+})();
